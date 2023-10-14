79a9edf714ae898748f97a2462ce43eabf25e350
chathurid <12209954@cqumail.com>
2023-10-15 01:33:41 +1000

    port updated

--- server/apiServer.js
+++ server/apiServer.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const app = express();
 const multer = require('multer');
 const baseURL = 'http://localhost:'
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json())
